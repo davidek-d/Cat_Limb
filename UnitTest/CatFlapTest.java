446eb69209ce0a3de0a02fc5bd38d20128a12451
BrammerN <k4wkv49@gmail.com>
2025-11-05 10:47:15 +0100

    Oprettelse af test metoder

--- UnitTest/CatFlapTest.java
+++ UnitTest/CatFlapTest.java
@@ -1,3 +1,32 @@
+import org.junit.jupiter.api.Test;
+
 public class CatFlapTest {
 
+    @Test
+    void getMode() {
+    }
+
+    @Test
+    void setMode() {
+    }
+
+    @Test
+    void canEnter() {
+    }
+
+    @Test
+    void canExit() {
+    }
+
+    @Test
+    void catRegistered() {
+    }
+
+    @Test
+    void registerCat() {
+    }
+
+    @Test
+    void getRegisteredCatsCount() {
+    }
 }
