97a9a18600d004b1fdc24864a8e8e1b289822b58
Namirah0310 <nare0001@stud.ek.dk>
2025-11-05 11:17:48 +0100

    validator not complete

--- src/CatFlapValidator.java
+++ src/CatFlapValidator.java
@@ -2,6 +2,10 @@
 
 public class CatFlapValidator {
     public static void chipIDCheck(){
+        try{
 
+        } catch (CatFlapExeption e){
+            System.out.println("Fejl: " + e.getMessage());
+        }
     }
 }
